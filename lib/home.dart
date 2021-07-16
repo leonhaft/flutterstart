325534c344bb519ba3bbce59d2a0189a9ef30257
leonhaft <929397+leonhaft@users.noreply.github.com>
2021-07-17 07:31:22 +0800

    绘制设置菜单

--- lib/home.dart
+++ lib/home.dart
@@ -39,16 +39,64 @@ class _HomePageState extends State<HomePage> {
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
+            //head
             UserAccountsDrawerHeader(
               accountName: Text("Michael"),
               accountEmail: Text("forza@outlook.com"),
               currentAccountPicture: GestureDetector(
                 child: CircleAvatar(
                   backgroundColor: Colors.green,
+                  //foregroundImage: AssetImage(),
                   child: Icon(Icons.person, color: Colors.white),
                 ),
               ),
+              decoration: new BoxDecoration(color: Colors.blue),
             ),
+            //body
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Home Page"),
+                  leading: Icon(Icons.home),
+                )),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("My Account"),
+                  leading: Icon(Icons.person),
+                )),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("My Orders"),
+                  leading: Icon(Icons.shopping_basket),
+                )),
+
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Categories"),
+                  leading: Icon(Icons.category),
+                )),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Favorites"),
+                  leading: Icon(Icons.favorite, color: Colors.red),
+                )),
+            Divider(),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(Icons.settings),
+                )),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("About"),
+                  leading: Icon(Icons.help),
+                )),
           ],
         ),
       ),
