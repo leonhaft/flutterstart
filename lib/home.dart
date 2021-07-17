0ea0e7daee98d14c8b7e5d4321814b3de591a6d5
leonhaft <929397+leonhaft@users.noreply.github.com>
2021-07-17 16:30:52 +0800

    修改轮播图片

--- lib/home.dart
+++ lib/home.dart
@@ -26,23 +26,87 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  final imgList = [
+    'images/carousels/1_386.png',
+    'images/carousels/1_388.png',
+    'images/carousels/1_515.png',
+    'images/carousels/1_535.png',
+    'images/carousels/2_265.png',
+    'images/carousels/3_310.png',
+    'images/carousels/black_1.png'
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final List<Widget> imageSliders = imgList
+        .map((item) => Container(
+              child: Container(
+                margin: EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Image.asset(item, fit: BoxFit.cover, width: 700.0),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Text(
+                              'No. ${imgList.indexOf(item)} image',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
+
+    final items = <Widget>[
+      Image.asset("images/carousels/1_386.png"),
+      Image.asset("images/carousels/1_388.png"),
+      Image.asset("images/carousels/1_515.png"),
+      Image.asset("images/carousels/1_535.png"),
+      Image.asset("images/carousels/2_265.png"),
+      Image.asset("images/carousels/3_310.png"),
+      Image.asset("images/carousels/black_1.png")
+    ];
+
     Widget carouselWidget = new Container(
       child: new CarouselSlider(
-          items: <Widget>[
-            Image.asset("images/carousels/1.jpg"),
-            Image.asset("images/carousels/2.jpg"),
-            Image.asset("images/carousels/3.jpg"),
-            Image.asset("images/carousels/4.jpg")
-          ],
+          items: imageSliders,
           options: new CarouselOptions(
-            height: 400,
-            aspectRatio: 16 / 9,
-            autoPlay: false,
-            autoPlayAnimationDuration: Duration(milliseconds: 800),
-            autoPlayCurve: Curves.fastOutSlowIn,
-          )),
+              height: 400,
+              aspectRatio: 16 / 9,
+              autoPlay: false,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              onPageChanged: (index, item) {
+                setState(() {
+                  _current = index;
+                });
+              })),
     );
     return Scaffold(
       appBar: AppBar(
@@ -118,7 +182,28 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: new ListView(
-        children: [carouselWidget],
+        children: [
+          carouselWidget,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: (Theme.of(context).brightness == Brightness.dark
+                              ? Colors.white
+                              : Colors.black)
+                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                ),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
